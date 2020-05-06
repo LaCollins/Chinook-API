d05241d817fe1266c21a70e277ab5dcba07b0cb8
Laura Collins <laura.e.collins1@gmail.com>
2020-05-05 19:12:28 -0500

    Add project files.

---
+++ Chinook-API/Controllers/CustomerController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Chinook_API.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chinook_API.Controllers
+{
+    [Route("api/customer")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        // GET: api/customer/country
+        [HttpGet("{country}")]
+        public IActionResult GetByCountry(string country)
+        {
+            var repo = new CustomerRepository();
+            var customers = repo.GetByCountry(country);
+
+            if (!customers.Any())
+                return NotFound();
+
+            return Ok(customers);
+        }
+    }
+}
---
+++ Chinook-API/Data/CustomerRepository.cs
@@ -0,0 +1,52 @@
+﻿using Chinook_API.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chinook_API.Data
+{
+    public class CustomerRepository
+    {
+        const string ConnectionString = "Server=localhost;Database=Chinook;Trusted_Connection=True";
+
+        public List<Customer> GetByCountry(string country)
+        {
+            var sql = @"
+                SELECT
+                    FirstName,
+                    LastName,
+                    CustomerId,
+                    Country
+                FROM Customer
+                WHERE Customer.Country = @Country";
+
+            using (var db = new SqlConnection(ConnectionString))
+            {
+                db.Open();
+
+                var cmd = db.CreateCommand();
+                cmd.CommandText = sql;
+                cmd.Parameters.AddWithValue("Country", country);
+
+                var reader = cmd.ExecuteReader();
+                var customers = new List<Customer>();
+                while (reader.Read())
+                {
+                    var customer = new Customer
+                    {
+                        FirstName = (string)reader["FirstName"],
+                        LastName = (string)reader["LastName"],
+                        CustomerId = (int)reader["CustomerId"],
+                        Country = (string)reader["Country"]
+                    };
+
+                    customers.Add(customer);
+                }
+
+                return customers;
+            }
+        }
+    }
+}
