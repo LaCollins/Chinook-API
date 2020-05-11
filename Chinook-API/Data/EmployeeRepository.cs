713336ee28297e3f9488a29e10696ffcf52dc0aa
Laura Collins <laura.e.collins1@gmail.com>
2020-05-11 18:53:57 -0500

    did sales agents and unique countries

---
+++ Chinook-API/Data/EmployeeRepository.cs
@@ -0,0 +1,27 @@
+﻿using Chinook_API.Model;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chinook_API.Data
+{
+    public class EmployeeRepository
+    {
+        const string ConnectionString = "Server=localhost\\SQLEXPRESS;Database=Chinook;Trusted_Connection=True";
+
+        public IEnumerable<Employee> GetSalesAgents()
+        {
+            var query = @"SELECT *
+                        FROM Employee
+                        WHERE Title like '%Agent'";
+
+            using (var db = new SqlConnection(ConnectionString))
+            {
+                return db.Query<Employee>(query);
+            }
+        }
+    }
+}
